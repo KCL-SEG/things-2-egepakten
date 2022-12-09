4f2743277d6b22fc361cad2413de7221261f80a5
Pakten, Kerem (k20057059) <kerem_ege.pakten@kcl.ac.uk>
2022-12-09 00:18:09 +0000

    input type fixed

--- things/models.py
+++ things/models.py
@@ -2,8 +2,8 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db import models
 
 class Thing(models.Model):
-    name = models.CharField(max_length=35, unique=True)
-    description = models.CharField(max_length=120, blank=True)
+    name = models.CharField(max_length=36, unique=True, blank=False)
+    description = models.CharField(max_length=121, blank=False)
     quantity = models.IntegerField(
         validators=[MinValueValidator(0),MaxValueValidator(50)]
     )
